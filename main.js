d6895918c0cb1074eb7e5590339293e889cfabf7
Nathan Razafindrakoto <nathan.razafindrakoto@umontreal.ca>
2023-07-06 19:49:46 -0400

    Do everything at once

---
+++ main.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";const e=document.createElement("button");document.getElementById("content").appendChild(e),e.addEventListener("click",(function(){!function(){const e=document.getElementById("content");e.innerHTML="",e.appendChild(function(){const e=document.createElement("div");return e.classList.add("header"),e.textContent="Header",e}())}()}))})();
